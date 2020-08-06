b8fccc26fa1fe9a5ff4fa6ae13487b17582e4e3c
Vincentfritsch <fritsch-vincent@hotmail.fr>
2020-08-06 13:57:42 +0200

    cutFirst function done

--- exercises/cut.js
+++ exercises/cut.js
@@ -11,9 +11,17 @@
  */
 
 // Your code:
-
+function cutFirst(str)
+{
+  if (str.length >2)
+  {return str.substr(2)}
+  else return "Entrez un mot d'au moins 3 lettres"
+}
 //* Begin of tests
 const assert = require('assert');
-
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof cutFirst, 'function');
+assert.strictEqual(cutFirst.length, 1);
+assert.strictEqual(cutFirst('abc'), 'c');
+assert.strictEqual(cutFirst('ab'), "Entrez un mot d'au moins 3 lettres");
+//assert.fail('You must write your own tests');
 // End of tests */
