a896524890c4c2157053c71decb0f3d287037c7c
Vincentfritsch <fritsch-vincent@hotmail.fr>
2020-08-06 13:36:57 +0200

    yell function done

--- exercises/yell.js
+++ exercises/yell.js
@@ -7,9 +7,15 @@
  */
 
 // Your code:
-
+function yell(str)
+{
+  return str.toUpperCase()
+}
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+//assert.fail('You must write your own tests');
+assert.strictEqual(typeof yell, 'function');
+assert.strictEqual(yell.length, 1);
+assert.strictEqual(yell('abC'), 'ABC');
 // End of tests */
