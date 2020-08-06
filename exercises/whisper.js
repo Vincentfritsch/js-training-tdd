bf6e6843aa053a277664c21b279608640f073094
Vincentfritsch <fritsch-vincent@hotmail.fr>
2020-08-06 13:41:14 +0200

    whisper function done

--- exercises/whisper.js
+++ exercises/whisper.js
@@ -8,9 +8,15 @@
  */
 
 // Your code:
-
+function whisper(str)
+{
+  return '*' + str.toLowerCase() + '*'
+}
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+//assert.fail('You must write your own tests');
+assert.strictEqual(typeof whisper, 'function');
+assert.strictEqual(whisper.length, 1);
+assert.strictEqual(whisper('ABC'), '*abc*');
 // End of tests */
